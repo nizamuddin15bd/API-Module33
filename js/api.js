8ba6edfb29ac74cc80e52eb9578e8d77b0f86fd9
nizamuddin15bd <nizamuddin15bd@gmail.com>
2022-10-19 14:42:08 +0600

    api33-5

--- js/api.js
+++ js/api.js
@@ -3,3 +3,20 @@ function loadDate() {
         .then(response => response.json())
         .then(Data => console.log(Data))
 }
+
+function loadUsers() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => displayUsers(data))
+}
+
+function loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
+// loadPosts();
+
+function displayUsers(data) {
+    console.log(data)
+}
