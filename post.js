ded711975c31f56a66961e9f9582f4b93681ca0c
nizamuddin15bd <nizamuddin15bd@gmail.com>
2022-10-19 18:21:21 +0600

    post03

--- post.js
+++ post.js
@@ -19,6 +19,24 @@ function displayPosts(posts) {
     }
 }
 
+
+
+function addAPost() {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringigy({
+            title: 'My new post',
+            body: 'This is my posts',
+            userId: 1,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        }
+    })
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
+
 // function loadPosts() {
 //     fetch('https://jsonplaceholder.typicode.com/posts')
 //         .then(res => res.json())
